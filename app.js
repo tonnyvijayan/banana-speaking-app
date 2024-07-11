442a828b86039dbae9ce44002cd08230c08801a3
tonnyvijayan <tonnyvijayan@gmail.com>
2024-07-11 17:44:46 +0530

    fix: fixed mobile responsiveness

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-/**declaring variables which will do input,processing,output & adding query selector */
-var txtInput = document.querySelector("#txt-input");
-var txtOutput = document.querySelector("#output");
-var buttonClick = document.querySelector("#btn-translate");
+const txtInput = document.querySelector("#txt-input");
+const txtOutput = document.querySelector("#output");
+const buttonClick = document.querySelector("#btn-translate");
 
-/**api server url */
-var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+const serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
-/**making the api query url  */
 function makeQueryUrl(text) {
-    return serverUrl + "?" + "text=" + text;
+  return serverUrl + "?" + "text=" + text;
+}
 
-};
-
-/**error handling message */
 function errorHandler(error) {
-    console.log("Error occured", error);
-    alert("Error encountere please try after sometime")
-};
+  alert("Error encountere please try after sometime");
+}
 
-/**making the function which will trigger when clicked */
 function clickHandler() {
-    var txtInputValue = txtInput.value;
+  const txtInputValue = txtInput.value;
 
-    fetch(makeQueryUrl(txtInputValue))
-        .then(respone => respone.json())
-        .then(json => {
-            var translatedText = json.contents.translated;
-            txtOutput.innerText = translatedText
-        })
-        .catch(errorHandler)    /**error handling */
-};
+  fetch(makeQueryUrl(txtInputValue))
+    .then((respone) => respone.json())
+    .then((json) => {
+      const translatedText = json.contents.translated;
+      txtOutput.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
 
-
-
-
-/**adding event listener to the button and calling the function */
 buttonClick.addEventListener("click", clickHandler);
